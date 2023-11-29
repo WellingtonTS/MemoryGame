90c72b09d2895be17eb1f3ef7a5fb1f735f88899
WellingtonTS <torquatojucas@gmail.com>
2023-11-29 18:57:50 -0300

    opdate logic game win for 24 cards

--- scripts/cards.js
+++ scripts/cards.js
@@ -54,7 +54,7 @@ function createCards() {
 
 function checkGameWin() {
   const disabledCards = document.querySelectorAll(".disabledCard");
-  if (disabledCards.length === 2) {
+  if (disabledCards.length === 24) {
     clearInterval(finishTimerInterval);
 
     const userDate = {
